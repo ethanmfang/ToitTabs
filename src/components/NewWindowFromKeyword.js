31476b96b40364c0bbe2a807dddc28b627f65034
ethanmfang <ethanmfang@gmail.com>
2020-08-12 19:54:14 -0600

    Commit 4

--- src/components/NewWindowFromKeyword.js
+++ src/components/NewWindowFromKeyword.js
@@ -2,12 +2,6 @@ import React from "react";
 
 const NewWindowFromKeyword = ({ tabList }) => {
   function clickHandler() {
-    //console.log("Keyword Window Initiated");
-    //console.log(tabList);
-
-    //log URLS from keyword
-    //let tabUrls = tabList.map((tab) => console.log(tab.props.tab.url));
-
     //log tab IDs from keyword
     tabIds = [];
     let tabIds = tabList.map((tab) => tab.props.tab.id);
